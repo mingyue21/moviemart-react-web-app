a640cb942bb8558ed681b7c0e24702cdcee7252e
renyongzheng <zheng.ren@northeastern.edu>
2023-04-13 13:44:09 -0700

    register screen

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,42 @@
 import React from "react";
+import {Form} from "antd";
+import Button from "../../components/Button";
+import "../../stylesheets/sizes.css";
+import {Link} from "react-router-dom";
 
 function Register() {
     return (
-        <div>Register</div>
+        <div className="flex justify-center h-screen items-center">
+            <div>
+                <h1 className="text-xl">MOVIEMART - REGISTER</h1>
+                <hr/>
+                <Form layout="vertical">
+                    <Form.Item
+                        label="Name"
+                        name="name"
+                        rules={[{required:true, message:"Please enter your name"}]}>
+                        <input type="text"/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{required:true, message:"Please enter your email"}]}>
+                        <input type="text"/>
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{required:true, message:"Please enter your password"}]}>
+                        <input type="text"/>
+                    </Form.Item>
+                    <div className="flex flex-column mt-2 gap-1">
+                        <Button
+                            fullWidth title="REGISTER" type='submit'/>
+                        <Link to="/login">Already have an account? Login</Link>
+                    </div>
+                </Form>
+            </div>
+        </div>
     )
 }
 

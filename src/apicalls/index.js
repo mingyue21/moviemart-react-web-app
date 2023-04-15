6d3ed0c3dbee068cff40c26c0f75d8300e547589
ZXY0827 <yang.zixuan@northeastern.edu>
2023-04-14 18:36:12 -0700

    movies part

--- src/apicalls/index.js
+++ src/apicalls/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
     headers : {
         'Content-Type' : 'application/json',
-        authorization: `Bearer  ${localStorage.getItem('token')}`
+        authorization : `Bearer ${localStorage.getItem('token')}`
         }
 
 });
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,18 +4,24 @@ import Button from "../../components/Button";
 import "../../stylesheets/sizes.css";
 import {Link, useNavigate} from "react-router-dom";
 import {RegisterUser} from "../../apicalls/users";
+import {useDispatch} from "react-redux";
+import {HideLoading, ShowLoading} from "../../redux/loadersSlice";
 
 function Register() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const onFinish = async(values) => {
         try {
+            dispatch(ShowLoading());
             const response = await RegisterUser(values);
+            dispatch(HideLoading());
             if(response.success){
                 message.success(response.message);
             }else{
                 message.error(response.message);
             }
         }catch (error){
+            dispatch(HideLoading());
             message.error(error.message);
         }
     };

9a0982f302a0a13d8d58990be2f601b4d6b2983b
mingyue21 <myyyyywang@gmail.com>
2023-05-02 12:59:42 -0700

    set deploy env variable

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 export const axiosInstance = axios.create({
+    baseURL : API_BASE,
     headers : {
         'Content-Type' : 'application/json',
         authorization : `Bearer ${localStorage.getItem('token')}`
